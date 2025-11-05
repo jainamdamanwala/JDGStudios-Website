bb81251716d3a31c21f595a59e7ec842ce1e5219
Jainam Damanwala <jainamdamanwala21@gmail.com>
2025-11-05 14:01:00 -0600

    Fixed folder name to run actions

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/JDG-studio-website/', // 👈 IMPORTANT
+  base: '/JDGStudios-Website/', // 👈 IMPORTANT
 })
